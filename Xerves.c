df58cb349999a8bcff8987dcc8eaab73e160ce4f
MahirBickBoi <mizanmahir6@gmail.com>
2023-04-10 17:30:07 +0600

    bitboard functions

--- Xerves.c
+++ Xerves.c
@@ -7,18 +7,9 @@ int main()
 
     U64 playBitBoard = 0ULL;
 
-    printf("Start:\n");
-    PrintBitBoard(playBitBoard);
-
     playBitBoard |= (1ULL << SQ64(D2));
-
-    printf("D2 added:\n");
-    PrintBitBoard(playBitBoard);
-
+    playBitBoard |= (1ULL << SQ64(D3));
     playBitBoard |= (1ULL << SQ64(G2));
-
-    printf("G2 added:\n");
-    PrintBitBoard(playBitBoard);
 
     return 0;
 }
